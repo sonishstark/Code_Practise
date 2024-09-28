0ec4ea0efd8cf5c1f5d4406ea3a4748169ca9df0
Sonish Stark <sonishstark@gmail.com>
2024-09-28 14:47:28 -0400

    comments

--- Day_1/odd_even.py
+++ Day_1/odd_even.py
@@ -1,8 +1,9 @@
 if __name__ == '__main__':
     n = int(input().strip())
-    
+#checking even or odd    
     if n % 2 != 0:
         print("Weird")
+#learned to write the range between ex: 2 to 5
     elif n % 2 == 0 and 2 <= n <= 5:
         print("Not Weird")
     elif n % 2 == 0 and 6 <= n <= 20:
