12c0477feea5481124492e7d797534d516c6a06e
Sonish Stark <sonishstark@gmail.com>
2024-09-29 17:30:29 -0400

    learned divison

---
+++ division.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # // gives whole number like INT
+    # /  gives decimal output like float(crct value)
+    print(a//b)
+    print(a/b)
+    
