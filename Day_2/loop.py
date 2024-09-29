04b3d614d2336a0eb0bdfc064e46ed2a2dc512dc
Sonish Stark <sonishstark@gmail.com>
2024-09-29 17:55:47 -0400

    added comment

--- Day_2/loop.py
+++ Day_2/loop.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
     n = int(input().strip())
+    #constrain
     if  1 <= n <= 20:
+        #how to enter for loop and i is incremented by default
         for i in range(n):
             print(i**2)
