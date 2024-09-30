46699ce99a68a799c01ac8e100ce50695f0ebac3
Sonish Stark <sonishstark@gmail.com>
2024-09-30 18:53:39 -0400

    learned range in loop and how to print in straight line

---
+++ printing.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    if 1 <= n <=150:
+        #by default it takes i from 0 so adding range in to loop 
+     for i in range(1, n+1):
+         #learned how to print in straight line ny adding end=''
+        print(i, end='')
