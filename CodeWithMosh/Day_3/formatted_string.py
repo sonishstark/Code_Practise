91396cdc2d93977ffa601afea7305aa8d897559a
Sonish Stark <sonishstark@gmail.com>
2024-11-13 08:56:48 -0500

    ended with methods

---
+++ CodeWithMosh/Day_3/formatted_string.py
@@ -0,0 +1,7 @@
+first = 'Sonish'
+last = 'Sasidharan'
+message = first + '[' + last + '] is a code'
+#formatted spring helps print complex statements 
+msg = f'{first} [{last}] is a coder'
+print(msg)
+print(message)
---
+++ CodeWithMosh/Day_3/methods_in_string.py
@@ -0,0 +1,14 @@
+methods = 'There are different types of methods'
+#gives length of character
+print(len(methods))
+#converts into capital letter
+print(methods.upper())
+#converts into small letter 
+print(methods.lower())
+#find the index value of that character
+print(methods.find('e'))
+print(methods.find('different'))
+#helps replace the words
+print(methods.replace('methods','built in methods'))
+#check for boolean in the statement
+print('are' in methods)
