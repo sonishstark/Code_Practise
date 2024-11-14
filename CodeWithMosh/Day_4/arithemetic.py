5270874a935f4ed93f4659da137bbf6ee6a7d152
sonish <sonishstark@gmail.com>
2024-11-14 14:11:40 -0500

    58:25 till math function

---
+++ CodeWithMosh/Day_4/arithemetic.py
@@ -0,0 +1,19 @@
+#return the reminder
+print(10/3)
+#returns the whole reminder 
+print(10//3)
+#returns the quotient 
+print(10%3)
+# ' ** ' acts a power
+print(10**3)
+
+x = 10
+x = x + 3
+#augmented assignment operator
+x += 3
+x -= 4
+print(x)
+
+#operator precendence AKA BODMAS  rule
+y = 10 + 3 * 2
+print(y) 
---
+++ CodeWithMosh/Day_4/math_functions.py
@@ -0,0 +1,5 @@
+x = 2.6
+#returns round of value
+print(round(x))
+#returns positive value 
+print(abs(-2.9))
