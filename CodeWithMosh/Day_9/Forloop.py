c9ee54bdcaa82115c02bd8f5971cb925859c83ee
Sonish Stark <sonishstark@gmail.com>
2024-11-19 17:54:55 -0500

    1:48:30

---
+++ CodeWithMosh/Day_9/Forloop.py
@@ -0,0 +1,23 @@
+#by default it takes no of elements in the string for iteration
+for item in 'Python':
+    print(item)
+print("")    
+#list is nothing set of items in '[]'
+for item in ['Sonish', 'Stark', 'ironman']:
+    print (item)
+print("")
+for item in [1,2,3,4]:
+    print(item)
+print('')
+
+# 'range' a built in fuction to set range/limit for the loop to execute
+for item in range(10):
+    print(item)
+print('')
+for item in range(5, 10):
+    print(item)
+print('')
+# skipping steps in between the range
+for item in range(5, 10, 2):
+    print(item)
+    
