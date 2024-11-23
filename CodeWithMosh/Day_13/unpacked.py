a3195101ef6d64418ef0fa830c411c4a35419fdd
sonish <sonishstark@gmail.com>
2024-11-23 16:36:55 -0500

    unpacked

---
+++ CodeWithMosh/Day_13/unpacked.py
@@ -0,0 +1,11 @@
+coordinates =(1, 2, 3)
+x = coordinates [2]
+print(x)
+#assigning values
+x, y, z= coordinates 
+print(y, z)
+
+#can also be done using list 
+coordinates = [4,5,6]
+x, y, z = coordinates
+print(z)
