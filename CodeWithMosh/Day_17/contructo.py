1407dde837214bf3da390bda85a455a49a4a190d
sonish <sonishstark@gmail.com>
2024-11-27 11:58:07 -0500

    3:14:55

--- CodeWithMosh/Day_17/contructo.py
+++ CodeWithMosh/Day_17/contructo.py
@@ -6,6 +6,10 @@ class Person:
     def talk(self):
         print(f"Hi, I am {self.name}")
 
-
+#object 1 for the same class
 john = Person("John smith")
 john.talk()
+
+#object 2 for the same class
+bob = Person("Bob smith")
+bob.talk()
