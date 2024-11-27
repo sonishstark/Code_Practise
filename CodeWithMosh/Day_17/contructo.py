ccc2134e86e7c9a2257def7857cae9dbe0b8227b
sonish <sonishstark@gmail.com>
2024-11-27 11:52:52 -0500

    constructor

---
+++ CodeWithMosh/Day_17/contructo.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def talk(self):
+        print("talk")
+
+
+john = Person("John smith")
+print(john.name)
+john.talk
