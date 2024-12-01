7c2c6fb94f11f210164042880b02079ef580067e
Sonish Stark <sonishstark@gmail.com>
2024-11-30 21:46:49 -0500

    randoms an inbuilt function

---
+++ CodeWithMosh/Day_20/diceroll.py
@@ -0,0 +1,11 @@
+import random
+ 
+class dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6)
+        return first, second
+    
+
+dice  = dice()
+print(dice.roll())
---
+++ CodeWithMosh/Day_20/randomvariable.py
@@ -0,0 +1,10 @@
+import random
+
+members = ['John', 'Mary', 'Bob', 'Mosh']
+leader = random.choice(members)
+print(leader)
+
+for i in range(3):
+    print(random.random())
+    print(random.randint(10,20))
+    
