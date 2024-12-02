635a6f61e865491bdca4a64ff89c8a6a03fe3180
Sonish Stark <sonishstark@gmail.com>
2024-12-02 10:05:09 -0500

    60 minutes

---
+++ EDX_Harvard/Day_22/parameters_arguments.py
@@ -0,0 +1,12 @@
+##remove spaces inbuilt functions 
+name = "  sonish sasidharan  ".strip().title()
+
+#the last argument ends the curson moving to next line
+print("Hello,",name, end="")
+print(" how are you")
+
+#printing removed spaces and capitalized user name 
+print(f"hello, {name}")
+#splitting name
+first, last = name.split()
+print(f"hello, {first}")
