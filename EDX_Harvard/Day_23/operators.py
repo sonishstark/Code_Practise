efc2c9e3f4a6b4a481fccdc1f28c74240621ddd0
sonish <sonishstark@gmail.com>
2024-12-06 12:27:20 -0500

    subraction

--- EDX_Harvard/Day_23/operators.py
+++ EDX_Harvard/Day_23/operators.py
@@ -1,4 +1,8 @@
-x = int(input("Enter a value: "))
-y = int(input("Enter a value: "))
+x = int(input("Enter the value for x: "))
+y = int(input("Enter the value for y: "))
 z = x + y
+#you can also integer the variable like this
+a = int(x) - int(y)
 print(z)
+print(a)
+print(x/y)
