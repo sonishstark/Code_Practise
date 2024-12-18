204407efa96acbe122eb370848abe6c17c0e91e6
Sonish Stark <sonishstark@gmail.com>
2024-12-18 05:27:54 -0500

    mniute changes

--- CodeWithMosh/Day_9/shoppingcart.py
+++ CodeWithMosh/Day_9/shoppingcart.py
@@ -2,4 +2,4 @@ prices = [10, 20, 30, 40]
 total = 0
 for item in prices:
     total += item
-print(total)
+print(total)            
--- EDX_Harvard/Day_25/date_and_time.py
+++
@@ -1,5 +0,0 @@
-
-#checking date and time
-
-#it worked, check 3
-# i will do it skipped
