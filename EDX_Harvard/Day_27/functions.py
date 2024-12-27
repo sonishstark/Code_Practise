71109c69aefeff7f78bb7fec33a5a50bce63fe1f
Sonish Stark <sonishstark@gmail.com>
2024-12-27 12:47:54 -0500

    adding multiple funcitons

--- EDX_Harvard/Day_27/functions.py
+++ EDX_Harvard/Day_27/functions.py
@@ -7,5 +7,13 @@ def main():
     #we can assign same variable, and a funciton to a variable
     guess = get_guess()
     print("The entered value is: ", guess)
+
+def main2():
+    guess = get_guess()
+    if guess == 50:
+        print("Correct")
+    else:
+        print("incorrect")
     
-main()
+    
+main2()
