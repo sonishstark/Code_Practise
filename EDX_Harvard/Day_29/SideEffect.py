82f2bd76e0cf572e7f99e0db4108cd71222e57d6
Sonish Stark <sonishstark@gmail.com>
2024-12-27 13:28:27 -0500

    Day_29

---
+++ EDX_Harvard/Day_29/SideEffect.py
@@ -0,0 +1,16 @@
+#global variable
+emoticon = "v.v"
+
+def main():
+    #we can modify the global variable 
+    global emoticon
+    say("Is anyone there?")
+    #moification only happening in the second function call
+    emoticon = ":)"
+    say("Oh, hi")
+
+def say(phrase):
+    print(phrase + " " + emoticon)
+
+#since the say funciton is inside the main function you dont have to call it separately again    
+main()
