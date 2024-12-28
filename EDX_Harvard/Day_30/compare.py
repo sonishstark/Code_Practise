5a4b0d2d61d07048ae7516a30f0dfd85514cba74
sonish <sonishstark@gmail.com>
2024-12-28 16:10:25 -0500

    Day 31

--- EDX_Harvard/Day_30/compare.py
+++ EDX_Harvard/Day_30/compare.py
@@ -1,15 +1,23 @@
+#global variables
+x = int(input("Enter a value of X: "))
+y = int(input("Enter a value of Y: "))
 
-def boolean():
-    
-    x = int(input("Enter a value of X: "))
-    y = int(input("Enter a value of Y: "))
+def conditional_1():
 
     if x < y:
         print("X is less than Y")
-    elif x == y:
-        print("X is equal Y")
-    else:
+    elif x > y:
         print("X is greater than Y")
+    else:
+        print("X is equal to Y")
 
-boolean ()
+#conditional_1()
 
+def conditional_2():
+    
+    if x > y or x < y:
+        print("X is not equal to Y")
+    else:
+        print("X is equal to Y")
+
+conditional_2()
---
+++ EDX_Harvard/Day_31/grade.py
@@ -0,0 +1,50 @@
+score = int(input("Enter the Score: "))
+
+def grading_1():
+    
+    if score >= 90 and score <=100:
+        print("Grade: A")
+    elif score >=80 and score < 90:
+        print("Grade: B")
+    elif score >=70 and score < 80:
+        print("Grade: C")
+    elif score >=60 and score < 70:
+        print("Grade: D")
+    else:
+        print("Grade: F")
+
+#grading_1()
+
+
+def grading_2():
+    
+    #creating a limit
+    if 90 <= score <=100:
+        print("Grade: A")
+    elif 80 <=score < 90:
+        print("Grade: B")
+    elif 70 <= score < 80:
+        print("Grade: C")
+    elif 60 <= score < 70:
+        print("Grade: D")
+    else:
+        print("Grade: F")
+    
+#grading_2()
+
+def grading_3():
+    
+    if score >= 90:
+        print("Grade: A")
+    elif score >= 80:
+        print("Grade: B")
+    elif score >= 70:
+        print("Grade: C")
+    elif score >= 60:
+        print("Grade: D")
+    else:
+        print("Grade: F")
+        
+grading_3()
+
+    
