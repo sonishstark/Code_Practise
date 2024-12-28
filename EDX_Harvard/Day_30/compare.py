1ed67317c84c8a2ad6c5962b701afbd80ed0ca76
sonish <sonishstark@gmail.com>
2024-12-28 14:30:15 -0500

    conditional statements

---
+++ EDX_Harvard/Day_30/compare.py
@@ -0,0 +1,15 @@
+
+def boolean():
+    
+    x = int(input("Enter a value of X: "))
+    y = int(input("Enter a value of Y: "))
+
+    if x < y:
+        print("X is less than Y")
+    elif x == y:
+        print("X is equal Y")
+    else:
+        print("X is greater than Y")
+
+boolean ()
+
