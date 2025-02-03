1b2335b4f90745d0a547481e7f45f056de9ffa50
Sonish Stark <sonishstark@gmail.com>
2025-02-03 16:38:16 -0500

    42

---
+++ EDX_Harvard/Day_41/mario1.py
@@ -0,0 +1,12 @@
+def main():
+    print_column(3)
+    print_row(4)
+    
+def print_row(width):
+    print("?" * width)
+
+def print_column(height):
+    for h in range(height):
+        print("#")
+
+main()
