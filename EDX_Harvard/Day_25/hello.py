337523c967fc6bf60402a6912598b0d7be2dc5c7
sonish <sonishstark@gmail.com>
2026-01-15 17:43:03 -0500

    changing main function

--- EDX_Harvard/Day_25/hello.py
+++ EDX_Harvard/Day_25/hello.py
@@ -1,10 +1,9 @@
-def main():
-    name = input("What's your name: ")
-    #adding the name through the user input
-    hello(name)
-    
 #passing the name in the function
 def hello(to):
     print(f"hello, {to}")
 
+def main():
+    name = input("What's your name: ")
+    #adding the name through the user input
+    hello(name)
 main()
